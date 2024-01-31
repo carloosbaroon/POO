d497448c89810287a07c4107761eeda54bc9d064
Carlos Baron <carlostectol@gmail.com>
2024-01-30 21:28:13 -0600

    Using some OOP principles
    - Polymorphism
    - Encapsulation

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,17 +1,79 @@
 public class Automovil {
 
-    String fabricante;
-    String modelo;
-    String color = "gris";
-    double cilindrada;
+    private String fabricante;
+    private String modelo;
+    private String color = "gris";
+    private double cilindrada;
+    private int capacidadTanque = 40;
 
-    public String detalle() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("auto.fabricante = " + this.fabricante);
-        sb.append("\nauto.modelo = " + this.modelo);
-        sb.append("\nauto.cilindrada = " + this.cilindrada);
-        sb.append("\nauto.color = " + this.color);
+    public String getFabricante() {
+        return this.fabricante;
+    }
 
-        return sb.toString();
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public double getCilindrada() {
+        return this.cilindrada;
+    }
+
+    public int getCapacidadTanque() {
+        return this.capacidadTanque;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public Automovil setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
+        return this;
+    }
+
+    public String verDetalle() {
+        return "auto.fabricante = " + this.fabricante +
+                "\nauto.modelo = " + this.modelo +
+                "\nauto.cilindrada = " + this.cilindrada +
+                "\nauto.color = " + this.color;
+    }
+
+    public String acelerar(int rpm) {
+        return "el auto " + this.fabricante + " acelerado a " + rpm + "rpm";
+    }
+
+    public String frenar() {
+        return this.fabricante + " " + this.modelo + " frenando!";
+    }
+
+    public String acelerarFrenar(int rpm) {
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+
+        return acelerar + "\n" + frenar;
+    }
+
+    public float calcularConsumo(int km, float porcentajeGas) {
+        return km/(capacidadTanque*porcentajeGas);
+    }
+
+    public float calcularConsumo(int km, int porcentajeGas) {
+        return km/(capacidadTanque*(porcentajeGas/100f));
     }
 }
--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -2,20 +2,27 @@ public class EjemploAutomovil {
     public static void main(String[] args) {
         
         Automovil subaru = new Automovil();
-        subaru.fabricante = "Subaru";
-        subaru.modelo = "Impresa";
-        subaru.cilindrada = 2.0;
-        subaru.color = "Blanco";
+        subaru.setFabricante("Subaru");
+        subaru.setModelo("Impresa");
+        subaru.setCilindrada(2.0);
+        subaru.setColor("Blanco");
 
         Automovil mazda = new Automovil();
-        mazda.fabricante = "Mazda";
-        mazda.modelo = "BT-50";
-        mazda.cilindrada = 3.0;
-        mazda.color = "Rojo";
+        mazda.setFabricante("Mazda");
+        mazda.setModelo("BT-50");
+        mazda.setCilindrada(3.0);
+        mazda.setColor("Rojo");
+        System.out.println("mazda.getFabricante() = " + mazda.getFabricante());
 
-        System.out.println(subaru.detalle());
-        System.out.println(mazda.detalle());
-        mazda.detalle();
+        System.out.println(subaru.verDetalle());
+        System.out.println(mazda.verDetalle());
+        System.out.println(subaru.acelerar(3000));
+        System.out.println(subaru.frenar());
+
+        System.out.println(mazda.acelerarFrenar(4000));
+
+        System.out.println("Kilometros por litro " + subaru.calcularConsumo(300, 0.6f));
+        System.out.println("Kilometros por litro " + subaru.calcularConsumo(300, 60));
 
     }
 }
