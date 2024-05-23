d8b9a86a80a285e0ba91db7871ff74fa33104417
Carlos Baron <carlostectol@gmail.com>
2024-05-23 13:04:27 -0600

    Using forward and redirect

---
+++ spring-boot-web-example/src/main/java/com/bolsaideas/springboot/web/app/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.bolsaideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        //This will "reset" our URL and redirect to /app/index
+        return "redirect:/app/index";
+        //This will not "reset" our URL and redirect to /app/index and will not allow us to forward to the "internet"
+        //return "forward:/app/index";
+    }
+}
