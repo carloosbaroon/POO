9d7ac47acde03903f44759a3626fbedb7b9bb303
Carlos Baron <carlostectol@gmail.com>
2024-05-25 11:12:31 -0600

    Inject dependencies
    Using:
    @Qualifier
    @Autowired

--- spring-boot-dependency-injection/src/main/java/com/bolsaideas/springboot/dependencyinjection/app/controllers/IndexController.java
+++ spring-boot-dependency-injection/src/main/java/com/bolsaideas/springboot/dependencyinjection/app/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.bolsaideas.springboot.dependencyinjection.app.controllers;
 
 import com.bolsaideas.springboot.dependencyinjection.app.services.IServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,25 @@ public class IndexController {
 
     //To inject an object that is registered in Spring's container
     @Autowired
+    //@Qualifier will allow us to inject a service with its own name
+    @Qualifier("miServicioComplejo")
     private IServicio servicio;
+
+    //We can omit the @Autowired annotation only in the constructor
+    /*@Autowired
+    public IndexController(IServicio servicio) {
+        this.servicio = servicio;
+    }*/
 
     @GetMapping({"/","/index"})
     public String index(Model model) {
         model.addAttribute("objecto", servicio.operacion());
         return "index";
     }
+
+    //Another way to inject a dependency using the setter
+    /*@Autowired
+    public void setServicio(IServicio servicio) {
+        this.servicio = servicio;
+    }*/
 }
--- spring-boot-dependency-injection/src/main/java/com/bolsaideas/springboot/dependencyinjection/app/services/MiServicioComplejo.java
+++ spring-boot-dependency-injection/src/main/java/com/bolsaideas/springboot/dependencyinjection/app/services/MiServicioComplejo.java
@@ -1,19 +1,15 @@
 package com.bolsaideas.springboot.dependencyinjection.app.services;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-
 
 //This is a singleton and will allow us to inject into other components of our system
 //This class will be a service -> business logic (DB, API REST, ETC)
 //@Service
-@Component("miServicioSimple")
-//This @Primary will make this class as the default one
-@Primary
-public class MiServicio implements IServicio {
+@Component("miServicioComplejo")
+public class MiServicioComplejo implements IServicio {
 
     @Override
     public String operacion(){
-        return "Ejecutando algun proceso importante simple";
+        return "Ejecutando algun proceso importante complicado";
     }
 }
