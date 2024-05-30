424ff82ca2ca4372947b0b4bc040745d34eb718c
Carlos Baron <carlostectol@gmail.com>
2024-05-30 09:32:19 -0600

    Adding error messages/validation
    One way to validate a form

--- spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
@@ -1,11 +1,16 @@
 package com.bolsaideas.springboot.form.app.controllers;
 
 import com.bolsaideas.springboot.form.app.models.domain.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -13,13 +18,27 @@ public class FormController {
     //To load the info into the form/browser
     @GetMapping("/form")
     public String form(Model model) {
+        Usuario usuario = new Usuario();
         model.addAttribute("titulo", "Formulario usuarios");
+        model.addAttribute("user", usuario);
         return "form";
     }
 
     @PostMapping("/form")
     //We can map the "usuario" with the form since it has the exact same attributes in the class and form.html
-    public String procesar(Usuario usuario, Model model){
+    //@Valid validates the attributes of "usuario" and BindingResult will provide us the result of that validation
+    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model){
+
+        model.addAttribute("titulo", "Resultado del form");
+
+        if (result.hasErrors()) {
+            Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(error -> {
+                errores.put(error.getField(), "El campo ".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()));
+            });
+            model.addAttribute("error", errores);
+            return "form";
+        }
         model.addAttribute("titulo", "Resultado del form");
         model.addAttribute("usuario", usuario);
         return "resultado";
--- spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/models/domain/Usuario.java
@@ -1,12 +1,17 @@
 package com.bolsaideas.springboot.form.app.models.domain;
 
+import jakarta.validation.constraints.NotEmpty;
+
 //This is an entity or POJO
 //We mustn't inject these values
 public class Usuario {
 
     //This attributes should match the ones in our views (html)
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String email;
 
     public String getUsername() {
