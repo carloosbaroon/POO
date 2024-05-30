83a57e8c01134683af0420e5757904bb7fcbd8f4
Carlos Baron <carlostectol@gmail.com>
2024-05-30 11:51:10 -0600

    Form with thymeleaf
    How to validate
    Keep concurrency of values
    Get values from object

--- spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/controllers/FormController.java
@@ -8,39 +8,41 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
+//This will allow us to get values that are not in the form for the post (Usuario.id)
+@SessionAttributes("usuario")
 public class FormController {
 
     //To load the info into the form/browser
     @GetMapping("/form")
     public String form(Model model) {
         Usuario usuario = new Usuario();
+        usuario.setNombre("Aleph");
+        usuario.setApellido("Baron");
+        usuario.setId("123.456.789-K");
         model.addAttribute("titulo", "Formulario usuarios");
-        model.addAttribute("user", usuario);
+        model.addAttribute("usuario", usuario);
         return "form";
     }
 
     @PostMapping("/form")
     //We can map the "usuario" with the form since it has the exact same attributes in the class and form.html
     //@Valid validates the attributes of "usuario" and BindingResult will provide us the result of that validation
-    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model){
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status){
 
         model.addAttribute("titulo", "Resultado del form");
 
+        //Here we are passing also the Class Usuario but with lower case -> "usuario" to the view form.html -> th:object="${usuario}"
+        //If we want to use a different name, we should use @ModelAttribute("user") and change it also in the view
         if (result.hasErrors()) {
-            Map<String, String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(error -> {
-                errores.put(error.getField(), "El campo ".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()));
-            });
-            model.addAttribute("error", errores);
             return "form";
         }
-        model.addAttribute("titulo", "Resultado del form");
+
         model.addAttribute("usuario", usuario);
+        status.setComplete();
         return "resultado";
     }
 }
--- spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsaideas/springboot/form/app/models/domain/Usuario.java
@@ -6,13 +6,42 @@ import jakarta.validation.constraints.NotEmpty;
 //We mustn't inject these values
 public class Usuario {
 
+    private String id;
     //This attributes should match the ones in our views (html)
+    @NotEmpty
+    private String nombre;
+    @NotEmpty
+    private String apellido;
     @NotEmpty
     private String username;
     @NotEmpty
     private String password;
     @NotEmpty
     private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
     public String getUsername() {
         return username;
