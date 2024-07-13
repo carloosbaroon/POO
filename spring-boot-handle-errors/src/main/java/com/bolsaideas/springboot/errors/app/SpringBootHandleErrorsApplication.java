1aa3c7605432aae2f0bec21884d912d6dbfb4a20
Carlos Baron <carlostectol@gmail.com>
2024-07-13 09:53:21 -0600

    Create new project and controller

---
+++ spring-boot-handle-errors/src/main/java/com/bolsaideas/springboot/errors/app/SpringBootHandleErrorsApplication.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springboot.errors.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHandleErrorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHandleErrorsApplication.class, args);
+    }
+
+}
---
+++ spring-boot-handle-errors/src/test/java/com/bolsaideas/springboot/errors/app/SpringBootHandleErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springboot.errors.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHandleErrorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
