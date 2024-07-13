b9435cdbffcaa19d2dbd54a91af14f031f836016
Carlos Baron <carlostectol@gmail.com>
2024-07-13 09:22:29 -0600

    Avoid exception
    This is something we should always do!!!

--- spring-boot-interceptor-horario/src/main/java/com/bolsaideas/springboot/interceptorhorario/app/interceptors/HorarioInterceptor.java
+++ spring-boot-interceptor-horario/src/main/java/com/bolsaideas/springboot/interceptorhorario/app/interceptors/HorarioInterceptor.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -42,6 +43,9 @@ public class HorarioInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         String mensaje = (String) request.getAttribute("mensaje");
-        modelAndView.addObject("horario", mensaje);
+
+        if (modelAndView != null && handler instanceof HandlerMethod) {
+            modelAndView.addObject("horario", mensaje);
+        }
     }
 }
