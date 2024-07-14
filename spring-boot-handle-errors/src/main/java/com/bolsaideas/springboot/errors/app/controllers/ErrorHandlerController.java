6bbffd1fed1c5e337f5ffd17e515fca4bbc72023
Carlos Baron <carlostectol@gmail.com>
2024-07-14 13:33:48 -0600

    Create new personalized numberFormatException

--- spring-boot-handle-errors/src/main/java/com/bolsaideas/springboot/errors/app/controllers/ErrorHandlerController.java
+++ spring-boot-handle-errors/src/main/java/com/bolsaideas/springboot/errors/app/controllers/ErrorHandlerController.java
@@ -16,6 +16,17 @@ public class ErrorHandlerController {
         model.addAttribute("message", ex.getMessage());
         model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
         model.addAttribute("timestamp", new Date());
+        //Here we could use the "generic" template
         return "error/arithmetic";
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    public String numberFormatError(NumberFormatException ex, Model model){
+        model.addAttribute("error", "Formato numero invalido!");
+        model.addAttribute("message", ex.getMessage());
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        model.addAttribute("timestamp", new Date());
+        //Here we could use the "generic" template
+        return "error/number-format";
     }
 }
