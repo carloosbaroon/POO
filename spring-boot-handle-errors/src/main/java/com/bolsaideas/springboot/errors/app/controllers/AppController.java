0d98bcb361d2b6c0883e28d6c3fd898173b4da34
Carlos Baron <carlostectol@gmail.com>
2024-07-15 09:40:38 -0600

    Use Optional and arrow functions to validate User
    In case we search by ID, we'll use an Option to simplify our code

--- spring-boot-handle-errors/src/main/java/com/bolsaideas/springboot/errors/app/controllers/AppController.java
+++ spring-boot-handle-errors/src/main/java/com/bolsaideas/springboot/errors/app/controllers/AppController.java
@@ -24,12 +24,13 @@ public class AppController {
 
     @GetMapping("/ver/{id}")
     public String ver(@PathVariable Integer id, Model model){
-        Usuario usuario = usuarioService.obtenerPorId(id);
+        //Usuario usuario = usuarioService.obtenerPorId(id);
 
-        if (usuario == null){
+        /*if (usuario == null){
             throw new UsuarioNotFoundException(id.toString());
-        }
+        }*/
 
+        Usuario usuario = usuarioService.obtenerPorIdOptional(id).orElseThrow(() -> new UsuarioNotFoundException(id.toString()));
         model.addAttribute("usuario", usuario);
         model.addAttribute("titulo", "Detalle usuario: ".concat(usuario.getNombre()));
         return "ver";
--- spring-boot-handle-errors/src/main/java/com/bolsaideas/springboot/errors/app/services/UsuarioService.java
+++ spring-boot-handle-errors/src/main/java/com/bolsaideas/springboot/errors/app/services/UsuarioService.java
@@ -3,9 +3,11 @@ package com.bolsaideas.springboot.errors.app.services;
 import com.bolsaideas.springboot.errors.app.models.domain.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioService {
 
     public List<Usuario> listar();
     public Usuario obtenerPorId(Integer id);
+    public Optional<Usuario> obtenerPorIdOptional(Integer id);
 }
