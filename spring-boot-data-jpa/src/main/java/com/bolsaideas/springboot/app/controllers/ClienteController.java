1e3b0f3a448995c52e503cce2bbcf77ff3faa3ae
Carlos Baron <carlostectol@gmail.com>
2024-07-20 10:25:18 -0600

    Save user in DAO and post method in controller

--- spring-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/controllers/ClienteController.java
@@ -1,11 +1,15 @@
 package com.bolsaideas.springboot.app.controllers;
 
 import com.bolsaideas.springboot.app.models.dao.IClienteDao;
+import com.bolsaideas.springboot.app.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
 
 @Controller
 public class ClienteController {
@@ -19,5 +23,20 @@ public class ClienteController {
         model.addAttribute("titulo", "Listado de clientes");
         model.addAttribute("clientes", clienteDao.findAll());
         return "listar";
+    }
+
+    @GetMapping(value = "/form")
+    public String crear(Map<String, Object> model){
+
+        Cliente cliente = new Cliente();
+        model.put("cliente", cliente);
+        model.put("titulo", "Formulario de Cliente");
+        return "form";
+    }
+
+    @PostMapping(value = "/form")
+    public String guardar(Cliente cliente){
+        clienteDao.save(cliente);
+        return "redirect:listar";
     }
 }
