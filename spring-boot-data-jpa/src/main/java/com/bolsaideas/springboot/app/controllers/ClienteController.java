36bb4615a7c7f5abc4017a4ef9dab9abfbb77f65
Carlos Baron <carlostectol@gmail.com>
2024-07-23 18:47:01 -0600

    Create EditarUsuario and use SessionAttributes
    SessionAttributes will allow us to get the cliente.id instead of using the a hidden field so we can edit an specific user

--- spring-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/controllers/ClienteController.java
@@ -8,13 +8,13 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import java.util.Map;
 
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
 
     @Autowired
@@ -37,16 +37,34 @@ public class ClienteController {
         return "form";
     }
 
+    @GetMapping(value = "/form/{id}")
+    public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model){
+
+        Cliente cliente = null;
+
+        if (id > 0) {
+            cliente = clienteDao.findOne(id);
+        } else {
+            return "redirect:/listar";
+        }
+
+        model.put("cliente", cliente);
+        model.put("titulo", "Editar Cliente");
+
+        return "form";
+    }
+
     @PostMapping(value = "/form")
     //We need to use @Valid to enable the validations in Cliente
     //We use ModelAttribute to send the object Cliente to the view, in this case we shouldn't do it
     // since it's named "Cliente" in crear(), this is just in case it's named different
-    public String guardar(@Valid @ModelAttribute("cliente") Cliente cliente, BindingResult result, Model model){
+    public String guardar(@Valid @ModelAttribute("cliente") Cliente cliente, BindingResult result, Model model, SessionStatus status){
         if (result.hasErrors()){
             model.addAttribute("titulo", "Formulario de Cliente");
             return "form";
         }
         clienteDao.save(cliente);
+        status.setComplete();
         return "redirect:listar";
     }
 }
