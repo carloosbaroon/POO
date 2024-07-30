dbf44bbe55734da4456a09c9a476fc3efacf890e
Carlos Baron <carlostectol@gmail.com>
2024-07-30 17:13:15 -0600

    Observable for ClienteService
    Listen for data emitted by the observable and when the observable emits data, the arrow function is called

--- spring-angular/clientes-app/src/app/clientes/cliente.service.ts
+++ spring-angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from "./cliente";
 import { CLIENTES } from "./clientes.json";
+import { Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ClienteService {
   constructor() {
   }
 
-  getClientes(): Cliente[] {
-    return CLIENTES
+  getClientes(): Observable <Cliente[]> {
+    return of(CLIENTES);
   }
 }
