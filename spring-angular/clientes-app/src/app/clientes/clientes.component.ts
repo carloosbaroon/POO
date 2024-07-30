f248a5a75f8aebc0af06cdb9de81eeb73929116b
Carlos Baron <carlostectol@gmail.com>
2024-07-30 10:54:40 -0600

    Create cliente.json

--- spring-angular/clientes-app/src/app/clientes/clientes.component.ts
+++ spring-angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
-import {Cliente} from "./cliente";
 import {NgForOf} from "@angular/common";
+import {Cliente} from "./cliente";
+import {CLIENTES} from "./clientes.json";
 
 @Component({
   selector: 'app-clientes',
@@ -12,12 +13,12 @@ import {NgForOf} from "@angular/common";
 })
 export class ClientesComponent {
 
-  clientes: Cliente[] = [
-    {id: 1, nombre: 'Aleph', apellido: 'Baron', email: 'abaron@email.com', createAt: '2024-29-07'},
-    {id: 2, nombre: 'Carlos', apellido: 'Baron', email: 'cbaron@email.com', createAt: '2024-29-07'},
-    {id: 3, nombre: 'Test', apellido: 'TestA', email: 'test@email.com', createAt: '1999-19-09'},
-    {id: 4, nombre: 'Magma', apellido: 'Lee', email: 'mlee@email.com', createAt: '2024-30-07'},
-    {id: 5, nombre: 'Dr James', apellido: 'Gosling', email: 'jgosling@email.com', createAt: '2024-31-07'}
-  ];
+  clientes: Cliente[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.clientes = CLIENTES;
+  }
 
 }
---
+++ spring-angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import {Cliente} from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Aleph', apellido: 'Baron', email: 'abaron@email.com', createAt: '2024-29-07'},
+  {id: 2, nombre: 'Carlos', apellido: 'Baron', email: 'cbaron@email.com', createAt: '2024-29-07'},
+  {id: 3, nombre: 'Test', apellido: 'TestA', email: 'test@email.com', createAt: '1999-19-09'},
+  {id: 4, nombre: 'Magma', apellido: 'Lee', email: 'mlee@email.com', createAt: '2024-30-07'},
+  {id: 5, nombre: 'Dr James', apellido: 'Gosling', email: 'jgosling@email.com', createAt: '2024-31-07'}
+];
