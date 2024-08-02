b405dd97d4167db9babd08d17c2cbfa75b40b571
Carlos Baron <carlostectol@gmail.com>
2024-08-02 09:39:30 -0600

    Create backend for Angular SAP
    I'm going to be using Spring boot for the backend of the existing Web app

---
+++ spring-angular-backend-apirest/src/main/java/com/bolsaideas/springangular/backend/apirest/SpringAngularBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springangular.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularBackendApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularBackendApirestApplication.class, args);
+    }
+
+}
---
+++ spring-angular-backend-apirest/src/test/java/com/bolsaideas/springangular/backend/apirest/SpringAngularBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springangular.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularBackendApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
