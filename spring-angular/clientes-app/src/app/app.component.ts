525d4f019b538f60794e46d5ce118ebe20c4aa81
Carlos Baron <carlostectol@gmail.com>
2024-08-01 20:04:03 -0600

    Enable routes

--- spring-angular/clientes-app/src/app/app.component.ts
+++ spring-angular/clientes-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from "./footer/footer.component";
 import { DirectiveComponent } from "./directive/directive.component";
@@ -8,7 +8,7 @@ import { ClientesComponent } from "./clientes/clientes.component";
 @Component({
   selector: 'app-root', // This will be the tag in html
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, DirectiveComponent, ClientesComponent,],
+  imports: [RouterLink, RouterOutlet, HeaderComponent, FooterComponent, DirectiveComponent, ClientesComponent],
   templateUrl: './app.component.html', // This is the view for this component
   styleUrls: ['./app.component.css'] // This is the style sheet
 })
--- spring-angular/clientes-app/src/app/app.routes.ts
+++ spring-angular/clientes-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {DirectiveComponent} from "./directive/directive.component";
+import {ClientesComponent} from "./clientes/clientes.component";
 
-export const routes: Routes = [];
+//Here we'll define our routes and 
+export const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
+  {path: 'directivas', component: DirectiveComponent},
+  {path: 'clientes', component: ClientesComponent},
+];
