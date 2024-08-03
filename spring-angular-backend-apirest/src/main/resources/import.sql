be97ea3d69d7c2b31d52ee1e6c74b3034da3a3fc
Carlos Baron <carlostectol@gmail.com>
2024-08-03 09:50:54 -0600

    Create endpoint to list all Clientes using a Controller

---
+++ spring-angular-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ALEPH', 'BARON', 'aleph@gmail.com', '2024-07-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('CARLOS', 'BARON', 'carlos@gmail.com', '2024-07-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('LETICIA', 'GONZALEZ', 'lety@gmail.com', '2024-08-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ANDRES', 'GUZMAN', 'andres@gmail.com', '2024-08-02');
