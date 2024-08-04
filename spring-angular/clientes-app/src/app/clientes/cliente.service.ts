b64f69cb2f9eb91a9f0418597a73344a26df502c
Carlos Baron <carlostectol@gmail.com>
2024-08-04 13:51:30 -0600

    Implement create() to save Cliente
    This will allow us to send the cliente from the form into the backend and save it into the DB

--- spring-angular/clientes-app/src/app/clientes/cliente.service.ts
+++ spring-angular/clientes-app/src/app/clientes/cliente.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Cliente } from "./cliente";
 import { CLIENTES } from "./clientes.json";
 import { Observable, of} from "rxjs";
@@ -11,11 +11,17 @@ export class ClienteService {
 
   private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
   constructor(private http: HttpClient) {
   }
 
   getClientes(): Observable <Cliente[]> {
     //return of(CLIENTES);
     return this.http.get<Cliente[]>(this.urlEndPoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndPoint, cliente, {headers: this.httpHeaders})
   }
 }
--- spring-angular/clientes-app/src/app/clientes/form.component.ts
+++ spring-angular/clientes-app/src/app/clientes/form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { Cliente } from "./cliente";
+import { ClienteService } from "./cliente.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-form',
@@ -14,9 +16,17 @@ export class FormComponent {
 
   public cliente: Cliente = new Cliente();
 
+  constructor(private clienteService: ClienteService,
+              private router: Router) {
+  }
+
+
   public create(): void{
     console.log("Create()");
     console.log(this.cliente)
+    this.clienteService.create(this.cliente).subscribe(
+      reponse => this.router.navigate(['/clientes'])
+    );
   }
 
 }
