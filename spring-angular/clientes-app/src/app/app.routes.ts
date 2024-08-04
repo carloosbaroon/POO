899ca9c539f4970a85db1d5a4b9d15f4db10ca40
Carlos Baron <carlostectol@gmail.com>
2024-08-04 13:34:51 -0600

    Missing files for the FormComponent to work

--- spring-angular/clientes-app/src/app/app.routes.ts
+++ spring-angular/clientes-app/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {DirectiveComponent} from "./directive/directive.component";
 import {ClientesComponent} from "./clientes/clientes.component";
+import {FormComponent} from "./clientes/form.component";
 
-//Here we'll define our routes and 
+//Here we'll define our routes and
 export const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectiveComponent},
   {path: 'clientes', component: ClientesComponent},
+  {path: 'clientes/form', component: FormComponent},
 ];
