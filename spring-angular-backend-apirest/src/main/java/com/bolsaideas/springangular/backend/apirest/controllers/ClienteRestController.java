f64a9a0c1157007ac9adf551fcc2c10089e55598
Carlos Baron <carlostectol@gmail.com>
2024-08-04 10:24:59 -0600

    Implement findById and save endpoints

--- spring-angular-backend-apirest/src/main/java/com/bolsaideas/springangular/backend/apirest/controllers/ClienteRestController.java
+++ spring-angular-backend-apirest/src/main/java/com/bolsaideas/springangular/backend/apirest/controllers/ClienteRestController.java
@@ -3,11 +3,10 @@ package com.bolsaideas.springangular.backend.apirest.controllers;
 import com.bolsaideas.springangular.backend.apirest.models.entity.Cliente;
 import com.bolsaideas.springangular.backend.apirest.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200/"})
@@ -21,5 +20,16 @@ public class ClienteRestController {
     @GetMapping("/clientes")
     public List<Cliente> index(){
         return clienteService.findAll();
+    }
+
+    @GetMapping("clientes/{id}")
+    public Cliente show(@PathVariable Long id){
+        return clienteService.findById(id);
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente){
+        return clienteService.save(cliente);
     }
 }
