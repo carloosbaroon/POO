645ed92c385b1ca5f5f8e227a75ea80c50d4dfa9
Carlos Baron <carlostectol@gmail.com>
2024-08-09 14:55:01 -0600

    Adding some styles
    For the html, we make it relative so all our rest of the components are absolutes

--- spring-angular-backend-apirest/src/main/resources/import.sql
+++ spring-angular-backend-apirest/src/main/resources/import.sql
@@ -2,3 +2,16 @@ INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ALEPH', 'BARO
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('CARLOS', 'BARON', 'carlos@gmail.com', '2024-07-29');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('LETICIA', 'GONZALEZ', 'lety@gmail.com', '2024-08-03');
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ANDRES', 'GUZMAN', 'andres@gmail.com', '2024-08-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ALEPH1', 'BARON', 'aleph@gmail.com', '2024-07-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('CARLOS1', 'BARON', 'carlos@gmail.com', '2024-07-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('LETICIA1', 'GONZALEZ', 'lety@gmail.com', '2024-08-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ANDRES1', 'GUZMAN', 'andres@gmail.com', '2024-08-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ALEPH2', 'BARON', 'aleph@gmail.com', '2024-07-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('CARLOS2', 'BARON', 'carlos@gmail.com', '2024-07-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('LETICIA2', 'GONZALEZ', 'lety@gmail.com', '2024-08-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ANDRES2', 'GUZMAN', 'andres@gmail.com', '2024-08-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ALEPH3', 'BARON', 'aleph@gmail.com', '2024-07-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('CARLOS3', 'BARON', 'carlos@gmail.com', '2024-07-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('LETICIA3', 'GONZALEZ', 'lety@gmail.com', '2024-08-03');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('ANDRES3', 'GUZMAN', 'andres@gmail.com', '2024-08-02');
+
