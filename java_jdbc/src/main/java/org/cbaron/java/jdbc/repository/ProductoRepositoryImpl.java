00ce6de85c42ae4e189dae8d669f8e882a3200ff
Carlos Baron <carlostectol@gmail.com>
2024-08-25 09:47:04 -0600

    Add save & delete operations

--- java_jdbc/src/main/java/org/cbaron/java/jdbc/repository/ProductoRepositoryImpl.java
+++ java_jdbc/src/main/java/org/cbaron/java/jdbc/repository/ProductoRepositoryImpl.java
@@ -1,7 +1,5 @@
 package org.cbaron.java.jdbc.repository;
 
-
-
 import org.cbaron.java.jdbc.models.Producto;
 import org.cbaron.java.jdbc.util.ConexionDB;
 
@@ -9,7 +7,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductoRepositoryImpl implements Repository{
+public class ProductoRepositoryImpl implements Repository<Producto> {
 
     private Connection getConnection() throws SQLException {
         return ConexionDB.getInstance();
@@ -36,19 +34,17 @@ public class ProductoRepositoryImpl implements Repository{
     }
 
     @Override
-    public Object findById(Long id) {
+    public Producto findById(Long id) {
         Producto producto = null;
 
         try (PreparedStatement preparedStatement = getConnection().
                 prepareStatement("SELECT * from productos where id = ?")) {
-
             preparedStatement.setLong(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                producto = crearProducto(resultSet);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    producto = crearProducto(resultSet);
+                }
             }
-            resultSet.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -58,12 +54,38 @@ public class ProductoRepositoryImpl implements Repository{
     }
 
     @Override
-    public void save(Object o) {
+    public void save(Producto producto) {
+        String sql = null;
+        if (producto.getId() != null && producto.getId() > 0) {
+            sql = "UPDATE productos SET nombre=?, precio=? WHERE id=?";
+        } else {
+            sql = "INSERT INTO productos(nombre, precio, fecha_registro) VALUES(?,?,?)";
+        }
+
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, producto.getNombre());
+            preparedStatement.setLong(2, producto.getPrecio());
+            if (producto.getId() != null && producto.getId() > 0) {
+                preparedStatement.setLong(3, producto.getId());
+            } else {
+                preparedStatement.setDate(3, new Date(producto.getFechaRegistro().getTime()));
+            }
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public void delete(Long id) {
+        try (PreparedStatement statement = getConnection().prepareStatement("DELETE FROM productos where id=?")) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
