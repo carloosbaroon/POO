f37cdd383420f7fc3bb22b213d46ce56c8941cab
Carlos Baron <carlostectol@gmail.com>
2024-09-12 13:58:43 -0600

    Create project for jdbc_transactions
    New project to use a singleton class with transactions for jdbc

---
+++ java_jdbc_singleton_transactions/src/main/java/org/cbaron/java/jdbc/EjemploJdbcTransactions.java
@@ -0,0 +1,54 @@
+package org.cbaron.java.jdbc;
+
+import org.cbaron.java.jdbc.models.Categoria;
+import org.cbaron.java.jdbc.models.Producto;
+import org.cbaron.java.jdbc.repository.ProductoRepositoryImpl;
+import org.cbaron.java.jdbc.repository.Repository;
+import org.cbaron.java.jdbc.util.ConexionDB;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class EjemploJdbcTransactions {
+    public static void main(String[] args) {
+
+        try (Connection connection = ConexionDB.getInstance()) {
+
+            Repository<Producto> repository = new ProductoRepositoryImpl();
+            System.out.println("--------------- Listar ---------------");
+            repository.findAll().forEach(System.out::println);
+
+            System.out.println("--------------- Obtener por ID ---------------");
+            System.out.println(repository.findById(2L));
+
+            System.out.println("--------------- Insertar nuevo producto ---------------");
+            Producto producto = new Producto();
+            producto.setNombre("Alimento Gato");
+            producto.setPrecio(900);
+            producto.setFechaRegistro(new Date());
+            Categoria categoria = new Categoria();
+            categoria.setId(1L);
+            producto.setCategoria(categoria);
+            producto.setSku("abcde12345");
+            repository.save(producto);
+            System.out.println("Producto guardado con exito");
+
+            System.out.println("--------------- Actualizar producto ---------------");
+            producto = new Producto();
+            producto.setId(1L);
+            producto.setNombre("Perchera Roja");
+            producto.setPrecio(1200);
+            producto.setSku("abcd123456");
+            categoria = new Categoria();
+            categoria.setId(2L);
+            producto.setCategoria(categoria);
+            repository.save(producto);
+            System.out.println("Producto actualizado con exito");
+            repository.findAll().forEach(System.out::println);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ java_jdbc_singleton_transactions/src/main/java/org/cbaron/java/jdbc/repository/ProductoRepositoryImpl.java
@@ -0,0 +1,111 @@
+package org.cbaron.java.jdbc.repository;
+
+import org.cbaron.java.jdbc.models.Categoria;
+import org.cbaron.java.jdbc.models.Producto;
+import org.cbaron.java.jdbc.util.ConexionDB;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoRepositoryImpl implements Repository<Producto> {
+
+    private Connection getConnection() throws SQLException {
+        return ConexionDB.getInstance();
+    }
+
+    @Override
+    public List<Producto> findAll() {
+        List<Producto> productos = new ArrayList<>();
+
+        try (Statement statement = getConnection().createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT p.*, c.nombre as categoria FROM productos as p " +
+                     "INNER JOIN categorias as c ON (p.categoria_id = c.id)")) {
+
+            while (resultSet.next()) {
+                Producto p = crearProducto(resultSet);
+
+                productos.add(p);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return productos;
+    }
+
+    @Override
+    public Producto findById(Long id) {
+        Producto producto = null;
+
+        try (PreparedStatement preparedStatement = getConnection().
+                prepareStatement("SELECT p.*, c.nombre as categoria FROM productos as p " +
+                        "INNER JOIN categorias as c ON (p.categoria_id = c.id) WHERE p.id = ?")) {
+            preparedStatement.setLong(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    producto = crearProducto(resultSet);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return producto;
+    }
+
+    @Override
+    public void save(Producto producto) {
+        String sql = null;
+        if (producto.getId() != null && producto.getId() > 0) {
+            sql = "UPDATE productos SET nombre=?, precio=?  ,categoria_id=?, sku=? WHERE id=?";
+        } else {
+            sql = "INSERT INTO productos(nombre, precio, categoria_id, sku, fecha_registro) VALUES(?,?,?,?,?)";
+        }
+
+        try (PreparedStatement preparedStatement = getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, producto.getNombre());
+            preparedStatement.setLong(2, producto.getPrecio());
+            preparedStatement.setLong(3, producto.getCategoria().getId());
+            preparedStatement.setString(4,producto.getSku());
+
+            if (producto.getId() != null && producto.getId() > 0) {
+                preparedStatement.setLong(5, producto.getId());
+            } else {
+                preparedStatement.setDate(5, new Date(producto.getFechaRegistro().getTime()));
+            }
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        try (PreparedStatement statement = getConnection().prepareStatement("DELETE FROM productos where id=?")) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static Producto crearProducto(ResultSet resultSet) throws SQLException {
+        Producto p = new Producto();
+        Categoria c = new Categoria();
+
+        p.setId(resultSet.getLong("id"));
+        p.setNombre(resultSet.getString("nombre"));
+        p.setPrecio(resultSet.getInt("precio"));
+        p.setFechaRegistro(resultSet.getDate("fecha_registro"));
+        p.setSku(resultSet.getString("sku"));
+        c.setId(resultSet.getLong("categoria_id"));
+        c.setNombre(resultSet.getString("categoria"));
+        p.setCategoria(c);
+        return p;
+    }
+}
