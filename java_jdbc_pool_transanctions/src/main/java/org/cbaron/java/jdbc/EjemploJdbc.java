f37e9bf3f136348caf5f9f7ab781cd5b7afc58f5
Carlos Baron <carlostectol@gmail.com>
2024-09-15 09:13:44 -0600

    Update Main to use Service classes

--- java_jdbc_pool_transanctions/src/main/java/org/cbaron/java/jdbc/EjemploJdbc.java
+++ java_jdbc_pool_transanctions/src/main/java/org/cbaron/java/jdbc/EjemploJdbc.java
@@ -5,6 +5,8 @@ import org.cbaron.java.jdbc.modelo.Producto;
 import org.cbaron.java.jdbc.repositorio.CategoriaRepositorioImpl;
 import org.cbaron.java.jdbc.repositorio.ProductoRepositorioImpl;
 import org.cbaron.java.jdbc.repositorio.Repositorio;
+import org.cbaron.java.jdbc.servicio.CatalogoServicio;
+import org.cbaron.java.jdbc.servicio.Service;
 import org.cbaron.java.jdbc.util.ConexionBaseDatos;
 
 import java.sql.*;
@@ -12,41 +14,25 @@ import java.util.Date;
 
 public class EjemploJdbc {
     public static void main(String[] args) throws SQLException {
-        try (Connection conn = ConexionBaseDatos.getConnection()) {
-            if (conn.getAutoCommit()) {
-                conn.setAutoCommit(false);
-            }
-            try {
-                Repositorio<Categoria> repositorioCategoria = new CategoriaRepositorioImpl(conn);
-                System.out.println("============= Insertar nueva categoria =============");
-                Categoria categoria = new Categoria();
-                categoria.setNombre("Conejos");
-                Categoria nuevaCategoria = repositorioCategoria.guardar(categoria);
-                System.out.println("Categoria guardada con éxito: " + categoria.getId());
-                Repositorio<Producto> repositorio = new ProductoRepositorioImpl(conn);
-                System.out.println("============= listar =============");
-                repositorio.listar().forEach(System.out::println);
 
-                System.out.println("============= obtener por id =============");
-                System.out.println(repositorio.porId(1L));
+        Service service = new CatalogoServicio();
+        System.out.println("============= listar =============");
+        service.listar().forEach(System.out::println);
+        
+        Categoria categoria = new Categoria();
+        categoria.setNombre("Peces");
 
-                System.out.println("============= insertar nuevo producto =============");
-                Producto producto = new Producto();
-                producto.setNombre("Alimento Conejo");
-                producto.setPrecio(600);
-                producto.setFechaRegistro(new Date());
-                producto.setSku("1234zxy");
+        System.out.println("============= insertar nuevo producto =============");
+        Producto producto = new Producto();
+        producto.setNombre("Alimento Pez");
+        producto.setPrecio(250);
+        producto.setFechaRegistro(new Date());
+        producto.setSku("1234zxyu");
 
-                producto.setCategoria(nuevaCategoria);
-                repositorio.guardar(producto);
-                System.out.println("Producto guardado con éxito: " + producto.getId());
-                repositorio.listar().forEach(System.out::println);
+        service.guardarProductoConCategoria(producto, categoria);
 
-                conn.commit();
-            } catch (SQLException e) {
-                conn.rollback();
-                e.printStackTrace();
-            }
-        }
+        System.out.println("Producto guardado con exito: " + producto.getId());
+        service.listar().forEach(System.out::println);
     }
 }
+
