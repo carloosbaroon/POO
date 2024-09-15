0199c7b310ffab8eead46e89ddfc305e0d635dbe
Carlos Baron <carlostectol@gmail.com>
2024-09-15 09:04:01 -0600

    Implement Service class
    This is like a facade for all our logic business!

--- java_jdbc_pool_transanctions/src/main/java/org/cbaron/java/jdbc/servicio/CatalogoServicio.java
+++ java_jdbc_pool_transanctions/src/main/java/org/cbaron/java/jdbc/servicio/CatalogoServicio.java
@@ -25,64 +25,107 @@ public class CatalogoServicio implements Service{
     public List<Producto> listar() throws SQLException {
         try (Connection conn = ConexionBaseDatos.getConnection()){
             productoRepositorio.setConn(conn);
+            return productoRepositorio.listar();
         }
-        return List.of();
     }
 
     @Override
     public Producto porId(Long id) throws SQLException {
         try (Connection conn = ConexionBaseDatos.getConnection()){
             productoRepositorio.setConn(conn);
+            return productoRepositorio.porId(id);
         }
-        return null;
     }
 
     @Override
     public Producto guardar(Producto producto) throws SQLException {
         try (Connection conn = ConexionBaseDatos.getConnection()){
             productoRepositorio.setConn(conn);
+            if (conn.getAutoCommit()) {
+                conn.setAutoCommit(false);
+            }
+
+            Producto nuevoProducto = null;
+
+            try {
+                nuevoProducto = productoRepositorio.guardar(producto);
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+            }
+            return nuevoProducto;
         }
-        return null;
     }
 
     @Override
     public void eliminar(Long id) throws SQLException {
         try (Connection conn = ConexionBaseDatos.getConnection()){
             productoRepositorio.setConn(conn);
+            if (conn.getAutoCommit()) {
+                conn.setAutoCommit(false);
+            }
+            try {
+                productoRepositorio.eliminar(id);
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+            }
         }
-
     }
 
     @Override
     public List<Categoria> listarCategoria() throws SQLException {
         try (Connection conn = ConexionBaseDatos.getConnection()){
             categoriaRepositorio.setConn(conn);
+            return categoriaRepositorio.listar();
         }
-        return List.of();
     }
 
     @Override
     public Categoria porIdCategoria(Long id) throws SQLException {
         try (Connection conn = ConexionBaseDatos.getConnection()){
             categoriaRepositorio.setConn(conn);
+            return categoriaRepositorio.porId(id);
         }
-        return null;
     }
 
     @Override
     public Categoria guardarCategoria(Categoria categoria) throws SQLException {
         try (Connection conn = ConexionBaseDatos.getConnection()){
             categoriaRepositorio.setConn(conn);
+            if (conn.getAutoCommit()) {
+                conn.setAutoCommit(false);
+            }
+
+            Categoria nuevaCategoria = null;
+            try {
+                nuevaCategoria = categoriaRepositorio.guardar(categoria);
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+            }
+            return nuevaCategoria;
         }
-        return null;
     }
 
     @Override
     public void eliminarCategoria(Long id) throws SQLException {
         try (Connection conn = ConexionBaseDatos.getConnection()){
             categoriaRepositorio.setConn(conn);
+            if (conn.getAutoCommit()) {
+                conn.setAutoCommit(false);
+            }
+            try {
+                categoriaRepositorio.eliminar(id);
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+            }
         }
-
     }
 
     @Override
@@ -90,7 +133,19 @@ public class CatalogoServicio implements Service{
         try (Connection conn = ConexionBaseDatos.getConnection()){
             productoRepositorio.setConn(conn);
             categoriaRepositorio.setConn(conn);
-        }
 
+            if (conn.getAutoCommit()) {
+                conn.setAutoCommit(false);
+            }
+            try {
+                Categoria nuevaCategoria = categoriaRepositorio.guardar(categoria);
+                producto.setCategoria(nuevaCategoria);
+                productoRepositorio.guardar(producto);
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+            }
+        }
     }
 }
