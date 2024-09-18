b3c69cd23dcfe5394ff4839aaddfd2cbdb9d2c36
Carlos Baron <carlostectol@gmail.com>
2024-09-18 13:02:55 -0600

    Create debito and credito methods with TDD
    Creating first unit tests, then implementing the logic in the methods debito and credito

--- junit_app/src/test/java/org/cbaron/junit5app/ejemplos/models/CuentaTest.java
+++ junit_app/src/test/java/org/cbaron/junit5app/ejemplos/models/CuentaTest.java
@@ -22,6 +22,7 @@ class CuentaTest {
     void testSaldoCuenta() {
         Cuenta cuenta = new Cuenta("Aleph", new BigDecimal("1000.12345"));
 
+        assertNotNull(cuenta.getSaldo());
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
@@ -33,5 +34,27 @@ class CuentaTest {
         Cuenta cuenta2 = new Cuenta("Aleph", new BigDecimal("8900.9997"));
 
         assertEquals(cuenta, cuenta2);
+    }
+
+    @Test
+    void testDebitoCuenta() {
+        Cuenta cuenta = new Cuenta("Aleph", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal("100"));
+
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+
+    }
+
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Aleph", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal("100"));
+
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+
     }
 }
