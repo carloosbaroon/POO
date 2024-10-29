651bf4d844aa25b1e2ca33f23a95d3f26dfcda27
Carlos Baron <carlostectol@gmail.com>
2024-10-28 20:46:15 -0600

    Use filter to create a new DB connection

---
+++ webapp-sessionFilter/src/main/java/org/cbaron/apiservlet/webapp/headers/filters/ConnectionFilter.java
@@ -0,0 +1,35 @@
+package org.cbaron.apiservlet.webapp.headers.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.cbaron.apiservlet.webapp.headers.util.ConnectionJDBC;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebFilter("/*")
+public class ConnectionFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        try (Connection connection = ConnectionJDBC.getConnection()) {
+            if (connection.getAutoCommit()) {
+                connection.setAutoCommit(false);
+            }
+            try {
+                servletRequest.setAttribute("connection", connection);
+
+                filterChain.doFilter(servletRequest, servletResponse);
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                ((HttpServletResponse) servletResponse).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
